0b9c6779d16b302b9b7f65d8104d9d66c9231002
bgroveben <bgroveben@gmail.com>
2016-07-16 03:03:02 -0700

    Make notes on migrations and update README

--- polls/models.py
+++ polls/models.py
@@ -8,3 +8,11 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+## Three step guide to making model changes:
+#1 Change your models (in models.py)
+#2 Run $ python manage.py makemigrations to create migrations for those changes
+#3 Run $ python manage.py migrate to apply those changes to the database
+
+## The reason that there are separate commands to make and apply migrations is because
+## you will commit migrations to your version control system and ship them with your app.
