976a9128072caa9d3b208471b0ad7f2c0e08f8ec
bgroveben <bgroveben@gmail.com>
2016-07-18 07:18:32 -0700

    Update results() view to deal with vote() view redirects

--- polls/views.py
+++ polls/views.py
@@ -14,8 +14,9 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You are looking at the results of question %s."
-    return HttpResponse(response % question_id)
+## After somebody votes in a question, the vote() view redirects to the results page for the question 
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
