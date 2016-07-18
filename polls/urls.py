af50770f28d1cc12d8bcfe75ac3898758e708dad
bgroveben <bgroveben@gmail.com>
2016-07-18 02:33:37 -0700

    Remove hardcoded URL in index.html template

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # example: /polls/
     url(r'^$', views.index, name='index'),
     # example: /polls/5/
+    # this is the 'name' value as called by the {% url %} template tag in index.html
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # example: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
