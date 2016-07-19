ff4f67f1ebf7e4bf5c8eb1d6ca084c6f9576f898
bgroveben <bgroveben@gmail.com>
2016-07-19 02:51:45 -0700

    Implement generic views -- Amend URLconf

--- polls/urls.py
+++ polls/urls.py
@@ -2,15 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'polls' # namespace URL name in case another app in the Django project has the same name 
+app_name = 'polls'
 urlpatterns = [
-    # example: /polls/
-    url(r'^$', views.index, name='index'),
-    # example: /polls/5/
-    # this is the 'name' value as called by the {% url %} template tag in index.html
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # example: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # example: /polls/5/vote/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
