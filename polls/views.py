14e536d5dcd89e7467c648a3a768f782919fdd0c
bgroveben <bgroveben@gmail.com>
2016-07-20 06:38:16 -0700

    Exclude posts set to be published in the future from the View

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -10,8 +11,10 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        # return the last five published questions
-        return Question.objects.order_by('-pub_date')[:5]
+        """return the last five published questions NOT including those set to be published in the future"""
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
