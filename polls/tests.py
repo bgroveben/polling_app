c8e21059a115923c7bca149cef6675c9c73eb4ae
bgroveben <bgroveben@gmail.com>
2016-07-20 06:44:04 -0700

    Write a shortcut function to create new questions

--- polls/tests.py
+++ polls/tests.py
@@ -1,8 +1,16 @@
 from django.test import TestCase
-
+from django.core.urlresolvers import reverse
 import datetime
 from django.utils import timezone
 from .models import Question
+
+def create_question(question_text, days):
+    """
+    Creates a question with the given 'question_text' and publishes the given number of 'days' offset to now
+    (negative for questions published in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.object.create(question_text=question_text, pub_date=time)
 
 class QuestionMethodTests(TestCase):
 
